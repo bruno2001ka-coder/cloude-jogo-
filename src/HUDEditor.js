9d203576ce4cc254327492da682a60ae05071d3c
bruno2001ka-coder <bruno2001ka@gmail.com>
2026-09-09 15:28:01 -0300

    feat: load player house with game modules

--- src/HUDEditor.js
+++ src/HUDEditor.js
@@ -2,6 +2,7 @@
 import'./GameplayPolish.js';
 import'./Performance.js';
 import'./PerformanceManager.js';
+import'./CasaJogador.js';
 // v2 separa retrato e paisagem. O v1 aplicava a mesma porcentagem nas duas orientacoes e fazia HUD,
 // radar e botoes se empilharem quando o celular girava.
 const CHAVE='quintal3d.hud.layout.v2';
